31683447bee7b71e3e6c23558237a3ab77225e60
Dalal Bin Humaid <dalalbinhumaid@gmail.com>
2022-03-13 20:25:25 +0300

    Generate sending email via cloud function

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config()
+
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+exports.sendEmail = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const email = req.body.data.email;
+        const name = req.body.data.name;
+        const password = req.body.data.password;
+
+        const msg = {
+            to: email,
+            from: 'checkly.services@gmail.com',
+            templateId: 'd-bdb551d3829d4735908b055a7284c134',
+            dynamicTemplateData: {
+                name: name,
+                password: password,
+                email: email
+            },
+        };
+        sgMail.send(msg).then(() => {
+            return res.status(200).send({
+                data: {
+                    status: 200,
+                    message: 'sent',
+                },
+            });
+        }, error => {
+            console.error(error);
+            if (error.res) {
+                return res.status(500).send({
+                    data: {
+                        status: 500,
+                        message: error.toString(),
+                    },
+                });
+            }
+        });
+    });
+});
