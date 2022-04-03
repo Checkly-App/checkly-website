1d9032ec4399f46c26f944841f1c6dc05e5a49f8
Dalal Bin Humaid <dalalbinhumaid@gmail.com>
2022-04-03 20:28:23 +0300

    Update Dashboard.js

--- src/pages/Company/Dashboard.js
+++ src/pages/Company/Dashboard.js
@@ -32,28 +32,67 @@ const Subtitle = styled.h1`
 const Container = styled.div`
     overflow-y: hidden;
     overflow-x: hidden;
-    min-height: 100vh;
-    padding: 3em 3rem;
+    padding: 3em 3rem 3em 3rem;
     grid-template-columns: 30% 30% 30%;
-    grid-template-rows: 10em 20em 20em 10em 10em;
+    grid-template-rows: 10em 20em;
     display: grid;
-    grid-template-areas:'cell1 cell2 cell3' 
-                        'cell4 cell4 cell3' 
-                        'cell5 cell6 cell7' 
-                        'cell8 cell8 cell9'
-                        'cell8 cell8 cell10';
+    grid-template-areas:'cell1 cell2 cell3' 'cell4 cell4 cell4';
     grid-gap: 2em;
     justify-content: center;
     @media (max-width: 768px) {
+        height: 100vh;
             padding: 1rem;
-            grid-gap: 1em;
+            grid-gap: 2em;
             grid-template-columns: 100%; 
-            grid-template-rows: repeat(2, 5%) 15% repeat(5, 10%) repeat(2, 5%);
-            grid-template-areas:'cell1' 'cell2' 'cell3' 
-                                'cell4' 'cell5' 'cell6' 
-                                'cell7' 'cell8' 'cell9' 
-                                'cell10';
+            grid-template-rows: 20% 20% 20% 40%;
+            grid-template-areas:'cell1' 'cell2' 'cell3' 'cell4';
     }
+`
+const Employees = styled.div`
+   grid-area: cell1;
+    background: linear-gradient(160deg, #47CEFF 70%, #2CB1EF 100%);
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+    border-radius: 1em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    justify-items: stretch;
+    padding: 2em;
+`;
+const Departments = styled.div`
+   grid-area: cell2;
+    background: linear-gradient(160deg, #D980FF 70%, #B86AD9 100%);
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+    border-radius: 1em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    justify-items: stretch;
+    padding: 2em;
+`;
+const Meetings = styled.div`
+   grid-area: cell3;
+    background: linear-gradient(160deg, #3DD2BB 70%, #26BEA7 100%);
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+    border-radius: 1em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    justify-items: stretch;
+    padding: 2em;
+`;
+const ChartContainer = styled.div`
+    padding: 2em 1em 1em 1em;
+    grid-area: cell4;
+    background: linear-gradient(165deg, #FFFFFF 70%, #F6F6F6 100%);
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+    border-radius: 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `
 
 const Dashboard = () => {
@@ -62,15 +101,12 @@ const Dashboard = () => {
     useEffect(() => {
         const hour = moment().hour();
 
-        if (hour > 16) {
+        if (hour > 16)
             setGreeting("Good evening");
-        }
-
-        if (hour > 11) {
-            return setGreeting("Good afternoon");
-        }
-
-        setGreeting('Good morning');
+        else if (hour > 11)
+            setGreeting("Good afternoon");
+        else
+            setGreeting('Good morning');
     }, []);
 
     return (
@@ -78,6 +114,16 @@ const Dashboard = () => {
             <Title>{greeting}</Title>
             <Subtitle>Acme Corporations</Subtitle>
             <Container>
+                <Employees>
+                </Employees>
+                <Departments>
+                </Departments>
+                <Meetings>
+
+                </Meetings>
+                <ChartContainer>
+
+                </ChartContainer>
             </Container>
         </Wrapper>
     );
