b9f96104c35e0de16c65ff2331c2e9d8a1b56722
Noura Alsulayfih <51097288+Nouraaalsulayfih@users.noreply.github.com>
2022-05-09 17:21:45 +0300

    added the attendance requirements
    added the status part

--- src/pages/Company/Employee/EmployeeProfile.js
+++ src/pages/Company/Employee/EmployeeProfile.js
@@ -5,15 +5,28 @@ import { database } from '../../../utilities/firebase';
 
 const EmployeeProfile = (props) => {
     
-    const [name, setName] = useState('d');
-    const [position, setPosition] = useState('d');
-    const [department, setDepartment] = useState('d');
-    const [email, setEmail] = useState('d');
-    const [phone, setPhone] = useState('d');
-    const [id, setId] = useState('d');
-    const [address, setAddress] = useState('d');
-    const [birthdate, setBirthdate] = useState('d');
-    const [gender, setGender] = useState('d');
+    const [name, setName] = useState('');
+    const [position, setPosition] = useState('');
+    const [departmentID, setDepartmentID] = useState('');
+    const [departmentName, setDepartmentName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [id, setId] = useState('');
+    const [address, setAddress] = useState('');
+    const [birthdate, setBirthdate] = useState('');
+    const [gender, setGender] = useState('');
+    const [checkIn, setCheckIn] = useState('');
+
+    let today = new Date();
+    var date = "";
+    if(parseInt(today.getMonth()+1) < 10){
+        date = "0" + parseInt(today.getMonth()+1) + "-";
+    }
+
+    if(today.getDate() < 10){
+        date = date + "0" + today.getDate() + "-";
+    }
+    date = date + today.getFullYear();
 
     //MARK: - search for employee with props.uid
     useEffect(() =>{
@@ -26,8 +39,8 @@ const EmployeeProfile = (props) => {
             const position = data.position;
             setPosition(position);
             //3-Employee department
-            const department = data.department;
-            setDepartment(department);
+            const departmentID = data.department;
+            setDepartmentID(departmentID);
             //4-Employee email
             const email = data.email;
             setEmail(email);
@@ -53,22 +66,34 @@ const EmployeeProfile = (props) => {
 
     //MARK: - fetching department info
     useEffect(() =>{
-        onValue(ref(database, 'Department/'+department), (snapshot) => {
+        onValue(ref(database, 'Department/'+departmentID), (snapshot) => {
             const data = snapshot.val();
             //9- Department name
-            const department = data.name;
-            setDepartment(department);
+            const dep = data.name;
+            setDepartmentName(dep);
         });
     });
 
+
+    //MARK: - fetching attendance info
+    useEffect(() =>{
+        onValue(ref(database, 'LocationAttendance/ emp'+props.uid+'-Attendance/'+date), (snapshot) => {
+            if(snapshot.val()['check-out'] != "TBD"){
+                setCheckIn("Employee is checked out");
+            }else{
+                setCheckIn("Emplyee is checked In");
+            }
+        });
+    });
 
     return (
         <div className='cardProfile'>
             <div className='upper-containerProfile'></div>
             <div className='lower-containerProfile'>
-                <h1 className="EmployeeProfileHeader">{name}</h1> <br></br>
+                <h1 className="EmployeeProfileHeader">{name}</h1><br></br>
+                <h4 className="EmployeeProfileSubHeader">Status:  </h4> <p className="employeeInfo attendance">{checkIn}</p><br></br><br></br>
                 <h4 className="EmployeeProfileSubHeader">Position:  </h4> <p className="employeeInfo">{position}</p><br></br><br></br>
-                <h4 className="EmployeeProfileSubHeader">Department:  </h4> <p className="employeeInfo">{department}</p><br></br><br></br>
+                <h4 className="EmployeeProfileSubHeader">Department:  </h4> <p className="employeeInfo">{departmentName}</p><br></br><br></br>
                 <h4 className="EmployeeProfileSubHeader">Email:  </h4> <p className="employeeInfo">{email}</p><br></br><br></br>
                 <h4 className="EmployeeProfileSubHeader">Phone number:  </h4> <p className="employeeInfo">{phone}</p><br></br><br></br>
                 <h4 className="EmployeeProfileSubHeader">Employee ID:  </h4> <p className="employeeInfo">{id}</p><br></br><br></br>
